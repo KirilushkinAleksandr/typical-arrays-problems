a28774fb83b09541cb9cf327c4c2bd7f2c16ecf0
KirilushkinAleksandr <alexander.kirilushkin@gmail.com>
2020-09-28 17:45:45 +0400

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
-exports.min = function min (array) {
+exports.min = function min (arr) {
+  if (arr && arr.length) {
+    return arr.reduce((res, item) => item < res ? item : res, arr[0]);
+  }
   return 0;
 }
 
-exports.max = function max (array) {
+exports.max = function max (arr) {
+  if (arr) {
+    return arr.reduce((res, item) => item > res ? item : res, 0);
+  }
   return 0;
 }
 
-exports.avg = function avg (array) {
+exports.avg = function avg (arr) {
+  if (arr && arr.length) {
+    return arr.reduce((res, item) => res + item, 0) / arr.length;
+  }
   return 0;
 }
